328772ee569fcbe68ca2554cc2ed4b8fbcf4534a
Yunsang You <78732806+YouYunsang@users.noreply.github.com>
2022-01-31 22:36:05 +0900

    Create printStr.cpp

---
+++ printStr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::string strData = "Test String";
+    
+    std::cout << "Sample String" << std::endl;
+    
+    std::cout << strData << std::endl;
+
+    strData = "Change String";
+
+    std::cout << strData << std::endl;
+
+    std::cout << "저는 " << 20 << "살" << "입니다." << std::endl;
+
+    return 0;
+}
